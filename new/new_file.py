bf53c2e6cee54a8f8e17e4b968896530418fc7e8
Zinhle <zkeswa023@student.wethinkcode.co.za>
2024-09-19 10:11:28 +0200

    Code cleanup; added functionality to filter jobs according to the user's desired post

--- new/new_file.py
+++ new/new_file.py
@@ -4,96 +4,73 @@ import requests
 # TODO: loop through the first 5 pages or so to get results, getting all 100 posts on each page
 # TODO: work on formatting string to required url string
 
+'''
+    main driver function
+'''
 def main():
     jobs = getJobs()           # get jobs from the database
-    # print("FOUND ++++++> ", jobs)
-    # print(type(jobs))
-
-    # print("FOUND IT ++++> ", jobs.get("results"))
-
-
-    filtered = filterJobs(jobs)
-
-    
-    
+    filtered = filterJobs(jobs, "Johannesburg")
+    filteredJobs = matchPost(filtered, "software engineer") # get all the jobs according to the user's job post
 
 
 
-
-
+'''
+    function makes API request to adzuna job webscraper API and returns the results    
+    @returnValue: (JSON object) a list of dictionaries of the jobs from the adzuna database
+'''
 def getJobs():
 
     url = "https://api.adzuna.com/v1/api/jobs/za/search/1"
-    # app_id = "95c5a470"
-    # app_key = "937982dda4b2b82969e27656088b9e0d"
-    # results_per_page = 20
-    # what_and = "software%20engineering"
-
     params = {"app_id":"95c5a470", "app_key":"937982dda4b2b82969e27656088b9e0d", "results_per_page": 20, "what_and":"software engineering"}
     result = requests.get(url, params=params)
-
     return result.json().get("results")
 
 
 
-
-def filterJobs(jobs):
-    resultingList = []                  # create a list for jobs matching the user location 
-
-
+'''
+    function appends jobs to list if they do match the user's location    
+    @jobs is a list of jobs returned from the database
+    @returnValue: a list of jobs matching user desired location
+'''
+def filterJobs(jobs, user_location):
+    listJobs = []                  # create a list for jobs matching the user location 
     for job in jobs:
-        resultJobs = matchJobLocation("Johannesburg", job)
-        if resultJobs:
-            resultingList.append(job)
+        matchesLocation = matchJobLocation(user_location, job)
+        if matchesLocation:
+            listJobs.append(job)
+    return listJobs
 
 
-    for r in resultingList:
-        print("@@@@@@", r)
-        
-        
-        
-        # print("=======> ", job)
 
-    # print("=======> ", resultJobs)
-    pass
-
-
+'''
+    function matches job location on for jobs on database to user's location
+    @returnValue: True if the job location matches user location, False if otherwise
+'''
 def matchJobLocation(target_location, result):
 
     job_location_str = result.get("location").get("display_name")
-    # print("###########", job_location_str)
-    
     res = "".join(job_location_str)                                           # get the location of the post
     location = res.split(",")[0]
-
     if location == target_location:
         return True
-
     return False
 
 
 
-    
-
-    pass
-
-
-
-
-
-
-# {"__CLASS__":"Adzuna::API::Response::JobSearchResults","count":8511,"mean":593139.52,}
-# {"__CLASS__":"Adzuna::API::Response::JobSearchResults","count":8512,"mean":593139.52}
-results = [{"latitude":-26.703988,"longitude":27.079099,"created":"2022-10-28T10:48:53Z","salary_is_predicted":"0","company":{"__CLASS__":"Adzuna::API::Response::Company","display_name":"OfferZen"},"description":"Software Engineer Are you a talented software engineer looking for your Dream Developer Job? OfferZen, the developer jobs marketplace, reverses the job search process. We connect developers with a vast network of top tech companies that are eager to apply to interview you This could be the last job posting you ever have to click on Why waste time applying to 100 different companies when you could build one OfferZen profile and have them come to you? You'll also receive upfront information about\u2026","redirect_url":"https://www.adzuna.co.za/land/ad/3633238261?se=iB6niMJ17xGoHIzMZVutLA&utm_medium=api&utm_source=95c5a470&v=E1E94E6532C7E6CA2DDAC0BD764C5D0055926DD9","location":{"area":["South Africa","North West","Southern","Potchefstroom"],"display_name":"Potchefstroom, Southern","__CLASS__":"Adzuna::API::Response::Location"},"title":"Software Engineer","__CLASS__":"Adzuna::API::Response::Job","id":"3633238261","category":{"__CLASS__":"Adzuna::API::Response::Category","label":"IT Jobs","tag":"it-jobs"},"adref":"eyJhbGciOiJIUzI1NiJ9.eyJpIjoiMzYzMzIzODI2MSIsInMiOiJpQjZuaU1KMTd4R29ISXpNWlZ1dExBIn0.hipGTC2Xn3hShXz8hgb4xmeltz9FMyFfU1CDfKRQyqw"}]
-
-
-
-# job_location_str = results[0].get("location").get("display_name")
-# res = "".join(job_location_str)                                           # get the location of the post
-# location = res.split(",")[0]
-
-
-# title = results[0].get("title")                                             # get the post title
+'''
+    function matches jobs according to the job post
+    @jobs: a list of jobs matching the user location
+    @returnValue: a list of jobs matching the user's desired job post
+'''
+def matchPost(jobs, desired_post):
+    matches = []
+    for job in jobs:
+        title = job.get("title")
+        # print("Title ===> ", desired_post in title.lower())
+        if desired_post in title.lower():
+            matches.append(job)
+    return matches
+            
 
 
 # company_name = results[0].get("company").get("display_name")                  # get company name
